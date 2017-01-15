cf228f35ed905fd4190d83c10f2853f8ca9e313a
Torr <torrpolakow@gmail.com>
2017-01-15 10:08:51 +0200

    learner is ready

--- part3_curious/LOOP.py
+++ part3_curious/LOOP.py
@@ -9,6 +9,7 @@ hls = hl[1]
 input_size = 2
 synapse_0, synapse_1, synapse_2, synapse_3 = lnr.initialize_synapses(hls, input_size)
 
+# for other runs
 for i in range(1000):
     l4_error, synapse_0, synapse_1, synapse_2, synapse_3 = lnr.learner(synapse_0, synapse_1, synapse_2, synapse_3)
     # print l4_error
